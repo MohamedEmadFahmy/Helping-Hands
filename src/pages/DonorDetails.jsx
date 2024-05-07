39e5b75053b64f71705a178c14e170885124ee1d
Mohamed <moamedamr584@gmail.com>
2024-05-07 04:23:26 +0300

    added empty navbar

--- src/pages/DonorDetails.jsx
+++ src/pages/DonorDetails.jsx
@@ -2,17 +2,21 @@
 
 import DonorCard from '../components/DonorCard';
 import FulfilledDonationsCard from '../components/FulfilledDonationsCard';
+import NavBar from '../components/NavBar';
 import { useState } from 'react';
 
 const DonorDetails = () => {
   const [viewDetails, setView] = useState(false);
 
   return (
-    <div className="flex justify-center items-center bg-secondaryShades-500 w-screen h-screen">
+    <div className="flex-col justify-center items-center bg-secondaryShades-500 w-screen h-screen">
+      <NavBar />
       {viewDetails ? (
-        <DonorCard />
+        <div className="flex justify-center items-center h-auto w-auto mt-60">
+          <DonorCard />
+        </div>
       ) : (
-        <div className="flex flex-col items-center w-full h-3/5 gap-5">
+        <div className="flex flex-col items-center w-full h-3/5 mt-60 gap-5">
           <FulfilledDonationsCard setView={setView} />
           <FulfilledDonationsCard setView={setView} />
           <FulfilledDonationsCard setView={setView} />
