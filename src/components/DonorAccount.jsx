ba12f8808fe9ea62e240a1a5b8efb2c154e6b796
YoussofEshrah <youssof_eshrah@icloud.com>
2024-05-07 15:26:12 +0300

    added volunteer information box to donor account page with 3 possible states:Regular,Doctor,Teacher

--- src/components/DonorAccount.jsx
+++ src/components/DonorAccount.jsx
@@ -15,7 +15,14 @@ const DonorAccount = () => {
     const [isPasswordEditing, setIsPasswordEditing] = useState(false);
     const [phoneNumber, setPhoneNumber] = useState("00000000000");
     const [isPhoneNumberEditing, setIsPhoneNumberEditing] = useState(false);
+    const [donorRole, setDonorRole] = useState("Teacher"); // Possible values: Regular, Doctor, Teacher
 
+    const [schedule, setSchedule] = useState("");
+    const [isScheduleEditing, setIsScheduleEditing] = useState(false);
+    const [specialty, setSpecialty] = useState("");
+    const [isSpecialtyEditing, setIsSpecialtyEditing] = useState(false);
+    const [subject, setSubject] = useState("");
+    const [isSubjectEditing, setIsSubjectEditing] = useState(false);
 
     const toggleUsernameEditing = () => {
         setIsUsernameEditing(!isUsernameEditing);
@@ -45,6 +52,18 @@ const DonorAccount = () => {
         setIsPhoneNumberEditing(!isPhoneNumberEditing);
     };
 
+    const toggleScheduleEditing = () => {
+        setIsScheduleEditing(!isScheduleEditing);
+    };
+
+    const toggleSpecialtyEditing = () => {
+        setIsSpecialtyEditing(!isSpecialtyEditing);
+    };
+
+    const toggleSubjectEditing = () => {
+        setIsSubjectEditing(!isSubjectEditing);
+    };
+
     const handleUsernameChange = (e) => {
         setUsername(e.target.value);
     };
@@ -57,7 +76,6 @@ const DonorAccount = () => {
         setLoc(e.target.value);
     };
 
-    
     const handleGenderChange = (e) => {
         setGender(e.target.value);
     };
@@ -72,6 +90,18 @@ const DonorAccount = () => {
 
     const handlePhoneNumberChange = (e) => {
         setPhoneNumber(e.target.value);
+    };
+
+    const handleScheduleChange = (e) => {
+        setSchedule(e.target.value);
+    };
+
+    const handleSpecialtyChange = (e) => {
+        setSpecialty(e.target.value);
+    };
+
+    const handleSubjectChange = (e) => {
+        setSubject(e.target.value);
     };
 
     const isValidEmail = (email) => {
@@ -105,10 +135,145 @@ const DonorAccount = () => {
         setIsPasswordEditing(false);
     };
 
+    const handleSaveSchedule = () => {
+        setIsScheduleEditing(false);
+    };
+
+    const handleSaveSpecialty = () => {
+        setIsSpecialtyEditing(false);
+    };
+
+    const handleSaveSubject = () => {
+        setIsSubjectEditing(false);
+    };
+
+    const renderVolunteerInformation = () => {
+        if (donorRole === "Regular") {
+            return (
+                <div className="bg-gray-200 p-4 rounded-lg shadow-md mt-12 w-1/2">
+                    <h2 className="text-lg font-bold mb-4">Volunteer Information</h2>
+                    <button className="text-blue-600 hover:text-blue-700 focus:outline-none">
+                        Volunteer Now!
+                    </button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="bg-gray-200 p-4 rounded-lg shadow-md mt-12 w-1/2">
+                    <h2 className="text-lg font-bold mb-4">Volunteer Information</h2>
+                    <div className="border-b border-gray-400 mb-4 pb-4">
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <p className="text-gray-600 mr-2">Role:</p>
+                                <p className="text-gray-800 font-semibold">{donorRole}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border-b border-gray-400 mb-4 pb-4">
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <p className="text-gray-600 mr-2">Schedule:</p>
+                                {isScheduleEditing ? (
+                                    <input
+                                        type="text"
+                                        value={schedule}
+                                        onChange={handleScheduleChange}
+                                        className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none"
+                                    />
+                                ) : (
+                                    <p className="text-gray-800 font-semibold">{schedule}</p>
+                                )}
+                            </div>
+                            <button
+                                onClick={toggleScheduleEditing}
+                                className="text-blue-600 hover:text-blue-700 focus:outline-none"
+                            >
+                                {isScheduleEditing ? "Save" : "Edit"}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="border-b border-gray-400 mb-4 pb-4">
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <p className="text-gray-600 mr-2">Location:</p>
+                                {isLocEditing ? (
+                                    <input
+                                        type="text"
+                                        value={loc}
+                                        onChange={handleLocChange}
+                                        className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none"
+                                    />
+                                ) : (
+                                    <p className="text-gray-800 font-semibold">{loc}</p>
+                                )}
+                            </div>
+                            <button
+                                onClick={toggleLocEditing}
+                                className="text-blue-600 hover:text-blue-700 focus:outline-none"
+                            >
+                                {isLocEditing ? "Save" : "Edit"}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mb-4 pb-4">
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <p className="text-gray-600 mr-2">{donorRole === "Doctor" ? "Specialty:" : "Subject(s):"}</p>
+                                {donorRole === "Doctor" ? (
+                                    <>
+                                        {isSpecialtyEditing ? (
+                                            <input
+                                                type="text"
+                                                value={specialty}
+                                                onChange={handleSpecialtyChange}
+                                                className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none"
+                                            />
+                                        ) : (
+                                            <p className="text-gray-800 font-semibold">{specialty}</p>
+                                        )}
+                                    </>
+                                ) : (
+                                    <>
+                                        {isSubjectEditing ? (
+                                            <input
+                                                type="text"
+                                                value={subject}
+                                                onChange={handleSubjectChange}
+                                                className="border border-gray-300 rounded-md px-2 py-1 focus:outline-none"
+                                            />
+                                        ) : (
+                                            <p className="text-gray-800 font-semibold">{subject}</p>
+                                        )}
+                                    </>
+                                )}
+                            </div>
+                            <div>
+                                {donorRole === "Doctor" ? (
+                                    <button
+                                        onClick={isSpecialtyEditing ? handleSaveSpecialty : toggleSpecialtyEditing}
+                                        className="text-blue-600 hover:text-blue-700 focus:outline-none"
+                                    >
+                                        {isSpecialtyEditing ? "Save" : "Edit"}
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={isSubjectEditing ? handleSaveSubject : toggleSubjectEditing}
+                                        className="text-blue-600 hover:text-blue-700 focus:outline-none"
+                                    >
+                                        {isSubjectEditing ? "Save" : "Edit"}
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    };
 
     return (
-        <div className="flex flex-col justify-center items-center w-screen">
-            <h1 className="text-3xl font-bold mb-4">Welcome, {username}</h1>
+        <div className="flex flex-col justify-center items-center w-screen bg-secondary p-10">
+            <h1 className="text-3xl mt-60 font-bold mb-10">Welcome, {username}</h1>
             <div className="bg-gray-200 p-4 rounded-lg shadow-md w-1/2">
                 <h2 className="text-lg font-bold mb-4">Personal Information</h2>
                 <div className="border-b border-gray-400 mb-4 pb-4">
@@ -207,7 +372,6 @@ const DonorAccount = () => {
                 </div>
             </div>
 
-
             <div className="bg-gray-200 p-4 rounded-lg shadow-md mt-12 w-1/2">
                 <h2 className="text-lg font-bold mb-4">Security Information</h2>
                 <div className="border-b border-gray-400 mb-4 pb-4">
@@ -281,6 +445,7 @@ const DonorAccount = () => {
                 </div>
             </div>
 
+            {renderVolunteerInformation()}
         </div>
     );
 };
