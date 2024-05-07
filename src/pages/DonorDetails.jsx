a4d858db03a5ba5e5fce83d487cc594e09792ee8
Mohamed <moamedamr584@gmail.com>
2024-05-07 04:01:22 +0300

    centered multiple fulfilled donations

--- src/pages/DonorDetails.jsx
+++ src/pages/DonorDetails.jsx
@@ -8,11 +8,16 @@ const DonorDetails = () => {
   const [viewDetails, setView] = useState(false);
 
   return (
-    <div className="flex justify-center bg-secondaryShades-500">
+    <div className="flex justify-center items-center bg-secondaryShades-500 w-screen h-screen">
       {viewDetails ? (
         <DonorCard />
       ) : (
-        <FulfilledDonationsCard setView={setView} />
+        <div className="flex flex-col items-center w-full h-3/5 gap-5">
+          <FulfilledDonationsCard setView={setView} />
+          <FulfilledDonationsCard setView={setView} />
+          <FulfilledDonationsCard setView={setView} />
+          <FulfilledDonationsCard setView={setView} />
+        </div>
       )}
     </div>
   );
