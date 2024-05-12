deaac2994b602e88c52e50580c8e28fbe2b91e87
Mohamed <moamedamr584@gmail.com>
2024-05-12 14:51:36 +0300

    added pending organization donations

--- src/components/OrganizationDonations.jsx
+++ src/components/OrganizationDonations.jsx
@@ -1,26 +1,56 @@
 /* eslint-disable react/prop-types */
-import OrganizationDonorDonationsCard from "./OrganizationDonorDonationsCard";
+
+import CheckMark from '../assets/images/checkmark.png';
+import OrganizationDonorDonationsCard from './OrganizationDonorDonationsCard';
+import Pending from '../assets/images/pending.png';
 
 const OrganizationDonations = () => {
-	return (
-		<div className="flex flex-col w-3/5 justify-center items-center gap-5">
-			<OrganizationDonorDonationsCard
-				name="Donation 1"
-				date="2022-01-01"
-				category="Category 1"
-			/>
-			<OrganizationDonorDonationsCard
-				name="Donation 2"
-				date="2022-02-01"
-				category="Category 2"
-			/>
-			<OrganizationDonorDonationsCard
-				name="Donation 3"
-				date="2022-03-01"
-				category="Category 3"
-			/>
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-3/5 justify-center items-center gap-5 pt-3 pb-3">
+      <OrganizationDonorDonationsCard
+        name="N/A"
+        date="N/A"
+        category="Medicine"
+        isFulfilled={false}
+        statusPic={Pending}
+      />
+      <OrganizationDonorDonationsCard
+        name="N/A"
+        date="N/A"
+        category="Toys"
+        isFulfilled={false}
+        statusPic={Pending}
+      />
+      <OrganizationDonorDonationsCard
+        name="N/A"
+        date="N/A"
+        category="Clothes"
+        isFulfilled={false}
+        statusPic={Pending}
+      />
+      <OrganizationDonorDonationsCard
+        name="N/A"
+        date="N/A"
+        category="Medicine"
+        isFulfilled={false}
+        statusPic={Pending}
+      />
+      <OrganizationDonorDonationsCard
+        name="Ghorza"
+        date="2022-02-01"
+        category="Electrical Appliances"
+        isFulfilled={true}
+        statusPic={CheckMark}
+      />
+      <OrganizationDonorDonationsCard
+        name="Emad"
+        date="2022-03-01"
+        category="Clothes"
+        isFulfilled={true}
+        statusPic={CheckMark}
+      />
+    </div>
+  );
 };
 
 export default OrganizationDonations;
--- src/pages/OrganizationDonationsPage.jsx
+++ src/pages/OrganizationDonationsPage.jsx
@@ -1,12 +1,12 @@
-import OrganizationDonations from "../components/OrganizationDonations";
+import OrganizationDonations from '../components/OrganizationDonations';
 
 const OrganizationDonationsPage = () => {
-	return (
-		<div className="flex flex-col justify-center items-center gap-5 bg-secondaryShades-500 w-full h-[90vh]">
-			<h1 className="text-5xl text-black font-bold">Donations</h1>
-			<OrganizationDonations />
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-center items-center gap-5 bg-secondaryShades-500 w-full min-h-[90vh]">
+      <h1 className="text-5xl text-black font-bold pt-3">Donations</h1>
+      <OrganizationDonations />
+    </div>
+  );
 };
 
 export default OrganizationDonationsPage;
